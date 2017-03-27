646141783e11fd86441227ad2b5e9fdca7a943f6
MohanaprabhuR <mohan@timeless.co>
2017-03-27 13:07:46 +0530

    first changed

---
+++ fonts.php
@@ -0,0 +1,15 @@
+<link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/66f0623f-1e9f-49a1-9a9b-e9a0ce6f0d0a.css"/>
+        <link href='https://fonts.googleapis.com/css?family=Source+Serif+Pro:400,600' rel='stylesheet' type='text/css'>
+			
+			
+				
+			
+
+          font-family: 'Brandon Text W01', Helvetica, sans-serif;
+          font-family: "Neue Helvetica W01", Helvetica, Arial, sans-serif;
+          body {
+          	-webkit-font-smoothing: antialiased !important;
+          }
+
+
+          https://css-tricks.com/rems-ems/
